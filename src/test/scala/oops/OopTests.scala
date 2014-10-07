93da1c6677178e1723a36addff7a120fc8214ca2
sajit <skunnumkal@amplify.com>
2014-10-07 17:35:04 -0400

    adding tests

--- src/test/scala/oops/OopTests.scala
+++ src/test/scala/oops/OopTests.scala
@@ -24,3 +24,31 @@ class OopTests extends FlatSpec with Matchers{
     c.equals(a) should be (true)
   }
 }
+case class Node(x:Char)
+case class Edge(n1:Node,n2:Node,value:Int)
+
+class CaseTests extends FlatSpec with Matchers {
+  it should "be sane" in {
+    true should be (true)
+  }
+  it should "check case class equality" in {
+    val n1 = Node('a')
+    val n2 = Node('a')
+    n1.equals(n2) should  be (true)
+    n2.equals(n1) should be (true)
+    Node('b').equals(Node('a')) should be (false)
+  }
+  it should "check case for edge class equality" in {
+    val e1 = Edge(Node('a'),Node('b'),4)
+    val e2 = Edge(Node('b'),Node('a'),4)
+    e1.equals(e2) should be (false)
+    e1.equals(Edge(Node('a'),Node('b'),4)) should be (true)
+  }
+
+  it should "check case class list equality" in {
+
+    val l1 = List(Edge(Node('a'),Node('b'),4))
+    val l2 = List(Edge(Node('a'),Node('b'),4))
+    l1.equals(l2) should be (true)
+  }
+}

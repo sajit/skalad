325a8caf7ece158f86c9931c3c1c61c1275014c2
sajit <skunnumkal@amplify.com>
2014-10-08 17:05:46 -0400

    Adding my own implementation of graphs

--- src/main/scala/data/structures/graphs/GraphBase.scala
+++ src/main/scala/data/structures/graphs/GraphBase.scala
@@ -7,6 +7,13 @@ package data.structures.graphs
 abstract class GraphBase[T, U] {
   case class Edge(n1: Node, n2: Node, value: U) {
     def toTuple = (n1.value, n2.value, value)
+    override def equals(o: Any) = o match {
+      case e:Edge => { ((n1.equals(e.n1) && n2.equals(e.n2)) || (n1.equals(e.n2) && n2.equals(e.n1))) &&
+        e.value.equals(value)
+      }
+      case _ => false
+    }
+    override def hashCode():Int = n1.hashCode()+n2.hashCode()+value.hashCode()
   }
   case class Node(value: T) {
     var adj: List[Edge] = Nil
@@ -21,8 +28,13 @@ abstract class GraphBase[T, U] {
   def edgeTarget(e: Edge, n: Node): Option[Node]
 
   override def equals(o: Any) = o match {
-    case g: GraphBase[T,U] => (nodes.keys.toSet -- g.nodes.keys.toSet == Set.empty &&
-      edges.map(_.toTuple).toSet -- g.edges.map(_.toTuple).toSet == Set.empty)
+    case g: GraphBase[T,U] => {
+//      println("Are edges equal?" + edges.toSet.equals(g.edges.toSet))
+//      println("This edges" + edges.toSet)
+//      println("Those edges " + g.edges.toSet)
+      ((nodes.keys.toSet -- g.nodes.keys.toSet == Set.empty) &&
+      edges.equals(g.edges))}
+      //edges.map(_.toTuple).toSet -- g.edges.map(_.toTuple).toSet == Set.empty)
     case _ => false
   }
 
@@ -39,6 +51,7 @@ abstract class GraphBase[T, U] {
 }
 
 class Graph[T, U] extends GraphBase[T, U] {
+
   override def equals(o: Any) = o match {
     case g: Graph[T,U] => super.equals(g)
     case _ => false

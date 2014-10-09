6209392a9fc9384115cd74bbcf0f98a94c75f376
sajit <skunnumkal@amplify.com>
2014-10-09 14:43:48 -0400

    Working my way on

--- src/main/scala/data/structures/graphs/MyGraph.scala
+++ src/main/scala/data/structures/graphs/MyGraph.scala
@@ -4,23 +4,65 @@ package data.structures.graphs
  * Created by skunnumkal on 10/8/14.
  */
 
-case class GraphNode[T](value: T){
-  var adj: List[GraphEdge[T]] = Nil
-  def addEdge(edge:GraphEdge[T]) = edge :: adj
-}
+case class GraphNode[T](value: T)
+//{
+//  var adj: List[GraphEdge[T]] = Nil
+//  def addEdge(edge:GraphEdge[T]) = edge :: adj
+//}
 
   //def neighbors: List[Node] = adj.map(edgeTarget(_, this).get)
 
 case class GraphEdge[T](n1:T,n2:T,weight:Int)
-class UndirectedGraph [T]{
-  var nodeMap:Set[GraphNode[T]] = Set.empty
-  def addNode(node:GraphNode[T]) = nodeMap + node
-  def addNodes(nodes:Set[GraphNode[T]]) = nodeMap ++ nodes
+class UndirectedGraph[T] extends MyGraph[T]{
 
-  def addEdge(n1:T,n2:T,weight:Int) = {
-    //if(nodeMap.contains(Graph))
-    val e1 = GraphEdge(n1,n2,weight)
-    val e2 = GraphEdge(n2,n1,weight)
-
+  override def addEdge(n1:T,n2:T,weight:Int) = {
+    addEdgeToNode(GraphNode(n1), GraphEdge(n1, n2, weight))
+    addEdgeToNode(GraphNode(n2), GraphEdge(n2, n1, weight))
   }
+
+}
+
+class DirectedGraph[T] extends MyGraph[T] {
+  override def addEdge(n1: T, n2: T, weight: Int): Unit = {
+    val node1 = GraphNode(n1)
+    val node2 = GraphNode(n2)
+    if(!nodeMap.contains(node2) || !nodeMap.contains(node1)){
+      throw new IllegalArgumentException("Node not found")
+    }
+    val e1 = GraphEdge(n1,n2,weight)
+    addEdgeToNode(node1,e1)
+  }
+}
+abstract class MyGraph[T] {
+  var nodeMap:Map[GraphNode[T],List[GraphEdge[T]]] = Map()
+  def addNode(node:GraphNode[T]) = nodeMap += node -> List()
+  def addNodes(nodes:Set[GraphNode[T]]) = nodeMap ++ nodes.map{aNode => (aNode,List())}.toSet
+
+  def addEdge(n1:T,n2:T,weight:Int)
+
+  def addEdgeToNode(node:GraphNode[T],edge:GraphEdge[T]) = {
+    nodeMap.get(node) match {
+      case Some(edges) => {
+        val updated:List[GraphEdge[T]] = edge :: edges
+        nodeMap += node -> updated
+      }
+      case None => throw  new IllegalArgumentException("Not found node " + node)
+    }
+  }
+
+  // If the edge E connects N to another node, returns the other node,
+  // otherwise returns None.
+  def edgeTarget(e: GraphEdge[T], n: GraphNode[T]): Option[GraphNode[T]] = {
+    nodeMap.get(n) match {
+      case Some(edges) => {
+
+        edges.contains(e) match {
+          case true => Some(GraphNode(e.n2))
+          case false => None
+        }
+      }
+      case _ => None
+    }
+  }
+
 }
--- src/test/scala/data/structures/graphs/MyGraphSpec.scala
+++ src/test/scala/data/structures/graphs/MyGraphSpec.scala
@@ -16,8 +16,23 @@ class MyGraphSpec extends FlatSpec with Matchers{
   it should "not distinguish same nodes with diff edges" in {
     val n1 = GraphNode('a')
     val n2 = GraphNode('a')
-    n2.addEdge(GraphEdge('a','b',4))
+    val graph = new DirectedGraph[Char]()
+    graph.addNode(n1)
+    graph.addEdge(n1.value,n2.value,5)
+
     n1.equals(n2) should be (true)
     n1.equals(GraphNode('b')) should be (false)
+  }
+
+  it should "add an edge to a graph" in {
+
+    val n1 = GraphNode('a')
+    val n2 = GraphNode('a')
+    val graph = new DirectedGraph[Char]()
+    graph.addNode(n1)
+    graph.addEdge(n1.value,n2.value,5)
+
+    graph.edgeTarget(GraphEdge(n1.value,n2.value,5),n1) should be (Some(n1))
+
   }
 }

aa84fb161fa486234e3132b9a6b2ee5ceb1dfd1c
Sajit Kunnumkal <sajit.kunnumkal@gmail.com>
2014-11-21 08:50:11 -0500

    wip

--- src/main/scala/data/structures/graphs/AdjMatrix.scala
+++ src/main/scala/data/structures/graphs/AdjMatrix.scala
@@ -9,8 +9,6 @@ class AdjMatrix(val matrix:Array[Array[Int]]) {
 
    private var visited:Array[Boolean] = Array.fill[Boolean](matrix.length)(false)
 
-   private val hdmd = new AdjacencyMatrix()
-
    private def doDfs(current:Int):Unit = {
      println("Current Node Idx" + current)
      visited(current) = true
@@ -21,10 +19,18 @@ class AdjMatrix(val matrix:Array[Array[Int]]) {
      }
    }
 
-  def dfs(start:Int):Unit = {
-    doDfs(start)
+  def getVisitedCount():Int = {
+    visited.foldRight(0)((el,sum) => if(el){ sum + 1} else {sum})
+
+  }
+
+  def resetVisited() = {
     //reset visited array
     visited = Array.fill[Boolean](matrix.length)(false)
+  }
+  def dfs(start:Int):Unit = {
+    doDfs(start)
+
   }
 
 
--- src/test/scala/data/structures/graphs/AdjMatrixSpec.scala
+++ src/test/scala/data/structures/graphs/AdjMatrixSpec.scala
@@ -12,17 +12,28 @@ class AdjMatrixSpec extends FlatSpec with Matchers{
 
 
   val graph = Array(Array(-1,-1,2,-1,-1),Array(-1,-1,-1,-1,3),Array(2,-1,-1,-1,-1),Array(-1,-1,-1,1,-1),Array(-1,3,-1,-1,-1))
-  val matrix = new AdjacencyMatrix()
   val matrix2 = new AdjMatrix(graph)
 
+  it should "init visited" in {
+    matrix2.getVisitedCount() should be (0)
+
+  }
+
   it should "dfs on a graph and print 2 nodes" in {
-        //val adjMatrix = AdjMatrix(graph)
+
         matrix2.dfs(0)
+        matrix2.getVisitedCount() should be (2)
+        matrix2.resetVisited()
+
     }
 
   it should "dfs on a graph and print 1 nodes" in {
     //val adjMatrix = AdjMatrix(graph)
     matrix2.dfs(3)
+    matrix2.getVisitedCount() should be (1)
+    matrix2.resetVisited()
   }
+
+
 
 }

e28b16270ea8bb258aef32f1c7ec2815dbec9a3b
sajit <skunnumkal@amplify.com>
2014-12-23 16:56:15 -0500

    KnightsTour broken

--- src/main/scala/misc/KnightsTour.scala
+++ src/main/scala/misc/KnightsTour.scala
@@ -8,59 +8,90 @@ import org.scalactic.Snapshot
 class KnightsTour (val size:Int) {
 
   val board = Array.ofDim[Boolean](size,size)
+  val tourStep = Array.ofDim[Int](size,size)
 
   def isComplete(snapshot:Array[Array[Boolean]]):Boolean = {
     snapshot.flatten.filter{ el => !el}.isEmpty
   }
 
+  var count = 0
+
+  //TODO refactor this
   def getValidNexts(xPos: Int, yPos: Int, snapshot: Array[Array[Boolean]]) : List[(Int,Int)] = {
     var result:List[(Int,Int)] = List()
     //topLeft
-    if(xPos-1>=0 && yPos-2>=0 && !board(xPos-1)(yPos-2)){
-      result = (xPos-1,yPos-2) :: result
+    result = if(xPos-1>=0 && yPos-2>=0 && !snapshot(xPos-1)(yPos-2)){
+      (xPos-1,yPos-2) :: result
+      }
+     else{
+      result
     }
-    if(xPos-2>=0 && yPos-1>=0 && !board(xPos-2)(yPos-1)){
-      result = (xPos-2,yPos-1) :: result
+    if(xPos-2>=0 && yPos-1>=0 && !snapshot(xPos-2)(yPos-1)){
+      result ::= (xPos-2,yPos-1)
     }
 
     //topRight
-    if(xPos-1>=0 && yPos+2<size && !board(xPos-1)(yPos+2)){
-      result = (xPos-1,yPos+2) :: result
+    if(xPos-1>=0 && yPos+2<size && !snapshot(xPos-1)(yPos+2)){
+      result ::= (xPos-1,yPos+2)
     }
-    if(xPos-2>=0 && yPos+1<size && !board(xPos-2)(yPos+1)){
-      result = (xPos-2,yPos+1) :: result
+    if(xPos-2>=0 && yPos+1<size && !snapshot(xPos-2)(yPos+1)){
+      result ::= (xPos-2,yPos+1)
     }
 
     //bottom right
-    if(xPos+1<size && yPos+2<size && !board(xPos+1)(yPos+2)){
-      result = (xPos+1,yPos+2) :: result
+    if(xPos+1<size && yPos+2<size && !snapshot(xPos+1)(yPos+2)){
+      result ::= (xPos+1,yPos+2)
     }
-    if(xPos+2<size && yPos+1<size && !board(xPos+2)(yPos+1)){
-      result = (xPos+2,yPos+1) :: result
+    if(xPos+2<size && yPos+1<size && !snapshot(xPos+2)(yPos+1)){
+      result ::= (xPos+2,yPos+1)
     }
 
     //bottomLeft
-    if(xPos+1<size && yPos-2>=0 && !board(xPos+1)(yPos-2)){
-      
+    if(xPos+1<size && yPos-2>=0 && !snapshot(xPos+1)(yPos-2)){
+     result ::= (xPos+1,yPos-2)
     }
+    if(xPos+2<size && yPos-1>=0 && !snapshot(xPos+2)(yPos-1)){
+      (xPos+2,yPos-1) :: result
+    }else{result}
 
-    result
+
   }
 
-  def knightsTour(xPos:Int,yPos:Int):Unit = {
+  def knightsTour(xPos:Int,yPos:Int,step:Int = 0):Unit = {
     board(xPos)(yPos) = true
+    tourStep(xPos)(yPos) = step
     if(isComplete(board)){
-      println("Completed a tour")
+      //println("Completed a tour")
+      count +=1
       return
     }
     else{
       val nextPositions:List[(Int,Int)] = getValidNexts(xPos,yPos,board)
       nextPositions.foreach{tuple => {
-        knightsTour(tuple._1,tuple._2)
-        board(tuple._1)(tuple._2) = false
+        knightsTour(tuple._1,tuple._2,step+1)
+        //board(tuple._1)(tuple._2) = false
       }}
 
     }
+   board(xPos)(yPos) = false
+
   }
+
+  def doWholeBoardTour() = {
+    board.zipWithIndex.foreach{
+      case(row,rowIdx) => row.zipWithIndex.foreach{case(col,colIdx) =>
+       knightsTour(rowIdx,colIdx,0)
+      }
+    }
+  }
+
+  def getTotalCount() = count
+
+  def printTourBoard = {
+    tourStep.foreach{ row => row.foreach{el => System.out.print(" " + el)}
+     System.out.println()
+    }
+  }
+
 
 }
---
+++ src/test/scala/misc/KnightsTourSpec.scala
@@ -0,0 +1,62 @@
+package misc
+
+import org.scalatest.{Matchers, FlatSpec}
+
+/**
+ * Created by skunnumkal on 12/23/14.
+ */
+class KnightsTourSpec extends FlatSpec with Matchers{
+
+  it should "get some tours" in {
+    val knightsTour = new KnightsTour(5)
+    knightsTour.getTotalCount() should be (0)
+    knightsTour.doWholeBoardTour()
+    println("# of tours for size 5 " + knightsTour.getTotalCount())
+    knightsTour.getTotalCount() should be (1728)
+    //knightsTour.printTourBoard
+  }
+
+  it should "not complete any tour on a small board" in {
+    val knightsTour = new KnightsTour(4)
+    knightsTour.knightsTour(0,0)
+    knightsTour.getTotalCount() should be (0)
+
+  }
+
+  it should "get only valid next Positions 1" in {
+    val integerSnapshot = Array(Array(0,9,-1,5,2),Array(13,4,1,10,-1),Array(8,-1,12,3,6),Array(-1,14,7,-1,11),Array(-1,-1,-1,-1,-1))
+    val booleanSnapshot:Array[Array[Boolean]] = integerSnapshot.map{row => row.map{ el => el >=0 }}
+
+    val knightsTour = new KnightsTour(5)
+    val nextSteps = knightsTour.getValidNexts(3,1,booleanSnapshot)
+    nextSteps.length should be (1)
+    nextSteps(0) should be ((4,3))
+
+
+  }
+
+  it should "get only valid next Positions 2" in {
+    val integerSnapshot = Array(Array(0,9,-1,5,2),Array(13,4,1,10,-1),Array(8,-1,12,3,6),Array(-1,14,7,-1,11),Array(-1,-1,-1,-1,-1))
+    val booleanSnapshot:Array[Array[Boolean]] = integerSnapshot.map{row => row.map{ el => el >=0 }}
+
+    val knightsTour = new KnightsTour(5)
+    val nextSteps = knightsTour.getValidNexts(0,2,booleanSnapshot)
+    nextSteps.length should be (2)
+
+
+
+  }
+
+  it should "get only valid next Positions 3" in {
+    val integerSnapshot = Array(Array(0,9,-1,5,2),Array(13,4,1,10,-1),Array(8,-1,12,3,6),Array(-1,14,7,-1,11),Array(-1,-1,-1,-1,-1))
+    val booleanSnapshot:Array[Array[Boolean]] = integerSnapshot.map{row => row.map{ el => el >=0 }}
+
+    val knightsTour = new KnightsTour(5)
+    val nextSteps = knightsTour.getValidNexts(4,4,booleanSnapshot)
+    nextSteps.isEmpty should be (true)
+
+
+
+  }
+
+}

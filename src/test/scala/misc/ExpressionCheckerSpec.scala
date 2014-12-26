8c78f14efeb5a6f814607cdeb0d1181eda18ceec
sajit <skunnumkal@amplify.com>
2014-12-26 15:31:10 -0500

    edge case tests

--- src/test/scala/misc/ExpressionCheckerSpec.scala
+++ src/test/scala/misc/ExpressionCheckerSpec.scala
@@ -11,4 +11,23 @@ class ExpressionCheckerSpec extends FlatSpec with Matchers{
     ExpressionChecker.isValid(expr) should be (true)
   }
 
+  it should "identify invalid infix" in {
+    ExpressionChecker.isValid("+11") should be (false)
+    ExpressionChecker.isValid("11+") should be (false)
+  }
+
+  it should "be Ok with long expression" in {
+    ExpressionChecker.isValid("1+3+4+3") should be (true)
+
+  }
+
+  it should "not support numbers > 9" in {
+    ExpressionChecker.isValid("1+34+3") should be (false)
+  }
+
+  it should "not be Ok with bad expressions" in {
+    ExpressionChecker.isValid("1+34/+3") should be (false)
+    ExpressionChecker.isValid("1+/3") should be (false)
+  }
+
 }

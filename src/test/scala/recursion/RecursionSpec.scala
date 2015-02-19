56f3353f55e114cfd8a90ad7fabba80f352b39aa
sajit <skunnumkal@amplify.com>
2015-02-19 15:22:28 -0500

    Take N

--- src/test/scala/recursion/RecursionSpec.scala
+++ src/test/scala/recursion/RecursionSpec.scala
@@ -15,4 +15,18 @@ class RecursionSpec extends FlatSpec with Matchers{
     RecursionUtils.maxVal(List[Integer](4,1,-5,3)) should be (4)
   }
 
+  it should "take 4 elements " in {
+    RecursionUtils.takeN(4,List(4,5,2,5,6,6,7,3))
+  }
+
+  it should "throw exception if take more" in {
+    try{
+      RecursionUtils.takeN(4,List(5,3,4))
+      fail()
+    }catch{
+      case re:RuntimeException => {}
+      case _:Throwable => fail()
+    }
+  }
+
 }

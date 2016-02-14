6c9291b965aab35b327e80d5164bb1e1db892f75
sajit <sajit.kunnumkal@gmail.com>
2016-02-13 19:27:50 -0500

    Module pattern 1

---
+++ js-design-patterns/module-patterns/objectliterals.js
@@ -0,0 +1,19 @@
+//In javascript there are many ways to implement modules
+//1. Object literal notation
+//2. The Module pattern
+//3. AMD modules
+//4. CommonJs modules
+//5. ECMAScript Harmony modules
+
+//Object literal notation
+
+var my1stModule = {
+	
+	name : 'Sample Module',
+	getName: function() {
+	  return this.name;
+	}
+};
+
+console.log('Module property',my1stModule.name);
+console.log('Module fn call', my1stModule.getName());
